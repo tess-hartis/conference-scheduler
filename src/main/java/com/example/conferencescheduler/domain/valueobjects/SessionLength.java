2460b537a38a732c22ce6ad69e07d5725da22f0a
tess-hartis <96891729+tess-hartis@users.noreply.github.com>
2022-04-02 10:53:30 -0500

    Remove comments

--- src/main/java/com/example/conferencescheduler/domain/valueobjects/SessionLength.java
+++ src/main/java/com/example/conferencescheduler/domain/valueobjects/SessionLength.java
@@ -11,25 +11,10 @@ public class SessionLength {
 
     public Integer value;
 
-    public SessionLength(Integer value){
+    private SessionLength(Integer value){
 
         this.value = value;
     }
-
-//    public static SessionLength of(Integer length) {
-//        if (length < 15) {
-//            throw new IllegalArgumentException("Invalid length");
-//        }
-//        if (length > 120) {
-//            throw new IllegalArgumentException("Invalid length");
-//        }
-//
-//        return new SessionLength(length);
-//    }
-//
-//    public Integer value() {
-//        return this.value;
-//    }
 
     public static Validation<String, SessionLength> validate(Integer length){
         if (length > 120 || length < 30)
