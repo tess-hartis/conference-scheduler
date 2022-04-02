881e640b38fde11d38993f7d3c1b2b0d6961eb0b
tess-hartis <96891729+tess-hartis@users.noreply.github.com>
2022-04-02 10:55:26 -0500

    Refactor controller to handle commands and queries

--- src/main/java/com/example/conferencescheduler/controllers/SessionsController.java
+++ src/main/java/com/example/conferencescheduler/controllers/SessionsController.java
@@ -1,14 +1,8 @@
 package com.example.conferencescheduler.controllers;
 
-import com.example.conferencescheduler.domain.Session;
-import com.example.conferencescheduler.domain.valueobjects.SessionDescription;
-import com.example.conferencescheduler.domain.valueobjects.SessionLength;
-import com.example.conferencescheduler.domain.valueobjects.SessionName;
+import com.example.conferencescheduler.cqrs.*;
 import com.example.conferencescheduler.dtos.GetSessionDto;
-import com.example.conferencescheduler.dtos.PostSessionDto;
-import com.example.conferencescheduler.repositories.SessionRepository;
-import io.vavr.control.Validation;
-import org.springframework.beans.BeanUtils;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,88 +11,73 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static io.vavr.API.*;
-import static io.vavr.Patterns.*;
+import static io.vavr.Patterns.$None;
+import static io.vavr.Patterns.$Some;
 import static org.springframework.http.ResponseEntity.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/sessions")
 public class SessionsController {
 
-    private final SessionRepository sessionRepository;
+    private final PostSessionHandler postSessionHandler;
+    private final PutSessionHandler putSessionHandler;
+    private final DeleteSessionHandler deleteSessionHandler;
+    private final GetSessionHandler getSessionHandler;
+    private final GetSessionsHandler getSessionsHandler;
 
-    public SessionsController(SessionRepository sessionRepository) {
-        this.sessionRepository = sessionRepository;
-    }
 
     @GetMapping
     public List<GetSessionDto> list() {
 
-        var sessions = sessionRepository.findAll();
-        return sessions.stream()
+        var request = new GetSessionsQuery();
+        var response = getSessionsHandler.handle(request);
+        return response.stream()
                 .map(GetSessionDto::fromSession)
                 .collect(Collectors.toList());
     }
 
     @GetMapping
     @RequestMapping("{id}")
-    public ResponseEntity<GetSessionDto> get(@PathVariable Long id) {
+    public ResponseEntity get(@PathVariable Long id) {
 
-        var input = sessionRepository.findByIdOption(id);
-        return Match(input).of(
+        var request = new GetSessionQuery(id);
+        var response = getSessionHandler.handle(request);
+        return Match(response).of(
                 Case($Some($()), x -> new ResponseEntity<>(GetSessionDto.fromSession(x), HttpStatus.OK)),
                 Case($None(), () -> new ResponseEntity<>(HttpStatus.NOT_FOUND)));
+
+//        return response.fold(notFound(), x -> new ResponseEntity<>(GetSessionDto.fromSession(x), HttpStatus.OK));
+//        return response.map(x -> ok(GetSessionDto.fromSession(x))).orElse(notFound());
+
     }
 
     @PostMapping
-    public ResponseEntity create(@RequestBody PostSessionDto dto) {
+    public ResponseEntity create(@RequestBody PostSessionCommand request) {
 
-        var name = SessionName.validate(dto.session_name);
-        var description = SessionDescription.validate(dto.session_description);
-        var length = SessionLength.validate(dto.session_length);
-
-        return Validation.combine(name, description, length)
-                .ap(Session::of)
-                .fold(e -> unprocessableEntity().body(e.toJavaList()),
-                      c -> ok(sessionRepository.saveAndFlush(c)));
-
-//                .map(sessionRepository::saveAndFlush)
-//                .mapError(Value::toJavaList);
-
-//        return Match(result).of(
-//                Case($Valid($()),() -> new ResponseEntity<>(HttpStatus.CREATED)),
-//                Case($Invalid($()), e -> new ResponseEntity<>(e, HttpStatus.BAD_REQUEST)));
-
+        var response = postSessionHandler.handle(request);
+        return response.fold(e -> unprocessableEntity().body(e), s -> ok(GetSessionDto.fromSession(s)));
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public ResponseEntity<HttpStatus> delete(@PathVariable Long id) {
 
-        var input = sessionRepository.deleteByIdInteger(id);
-        return Match(input).of(
-                Case($(0), new ResponseEntity<>(HttpStatus.BAD_REQUEST)),
+        var request = new DeleteSessionCommand(id);
+        var response = deleteSessionHandler.handle(request);
+        return Match(response).of(
+                Case($(0), new ResponseEntity<>(HttpStatus.NOT_FOUND)),
                 Case($(1), new ResponseEntity<>(HttpStatus.NO_CONTENT)));
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public ResponseEntity update(@PathVariable Long id, @RequestBody PostSessionDto dto) {
+    public ResponseEntity update(@PathVariable Long id, @RequestBody PutSessionCommand request) {
 
-        var existingSession = sessionRepository.findByIdOption(id);
-
-        var name = SessionName.validate(dto.session_name);
-        var description = SessionDescription.validate(dto.session_description);
-        var length = SessionLength.validate(dto.session_length);
-
-        var result = existingSession
-                .map(s -> Validation.combine(name, description, length)
-                        .ap(s::Update));
-
-        return Match(result).of(
-                Case($Some($()),
-                        x -> x.fold(e -> unprocessableEntity().body(e.toJavaList()),
-                                s -> ok(sessionRepository.saveAndFlush(s)))),
+        request.id = id;
+        var response = putSessionHandler.handle(request);
+        return Match(response).of(
+                Case($Some($()), x ->
+                        x.fold(e -> unprocessableEntity().body(e), s -> ok(GetSessionDto.fromSession(s)))),
                 Case($None(), () -> new ResponseEntity<>(HttpStatus.NOT_FOUND)));
-
-//        BeanUtils.copyProperties(session, existingSession, "session_id");
-//        sessionRepository.saveAndFlush(existingSession);
     }
+
 }
