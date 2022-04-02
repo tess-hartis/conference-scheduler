9c30b4e0a86bda327cece00922f4b41e91934623
tess-hartis <96891729+tess-hartis@users.noreply.github.com>
2022-04-02 15:54:20 -0500

    Rename class and variables

--- src/main/java/com/example/conferencescheduler/domain/valueobjects/NameTitle.java
+++ src/main/java/com/example/conferencescheduler/domain/valueobjects/NameTitle.java
@@ -2,19 +2,19 @@ package com.example.conferencescheduler.domain.valueobjects;
 
 import io.vavr.control.Validation;
 
-public class SessionName{
+public class NameTitle {
 
     public String value;
 
-    private SessionName(String value){
+    private NameTitle(String value){
         this.value = value;
     }
 
-    public static Validation<String, SessionName> validate(String name){
+    public static Validation<String, NameTitle> validate(String name){
         if (name.isEmpty())
             return Validation.invalid("Name cannot be empty");
 
-        else return Validation.valid(new SessionName(name));
+        else return Validation.valid(new NameTitle(name));
     }
 
 }
