1fa7121f08980dccc4997112717ff012df3447a8
tess-hartis <96891729+tess-hartis@users.noreply.github.com>
2022-04-02 11:13:18 -0500

    Remove value annotation

--- src/main/java/com/example/conferencescheduler/domain/valueobjects/SessionDescription.java
+++ src/main/java/com/example/conferencescheduler/domain/valueobjects/SessionDescription.java
@@ -1,11 +1,9 @@
 package com.example.conferencescheduler.domain.valueobjects;
 
 import io.vavr.control.Validation;
-import lombok.Value;
 
 import java.util.Objects;
 
-@Value
 public class SessionDescription {
 
     public String value;
