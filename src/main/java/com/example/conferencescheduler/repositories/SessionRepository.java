c1d6231ad02846a8bec2fc9339f2d7014fab3bff
tess-hartis <96891729+tess-hartis@users.noreply.github.com>
2022-04-02 10:52:27 -0500

    Add repository annotation

--- src/main/java/com/example/conferencescheduler/repositories/SessionRepository.java
+++ src/main/java/com/example/conferencescheduler/repositories/SessionRepository.java
@@ -2,6 +2,8 @@ package com.example.conferencescheduler.repositories;
 
 import com.example.conferencescheduler.domain.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SessionRepository extends JpaRepository<Session, Long>, SessionRepositoryCustom {
 }
